97a16cd544e1b25b10ac27b20fc5f1b3a32a37a1
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-24 12:29:44 -0400

    add sessionModel to backend

---
+++ backend/models/sessionModel.js
@@ -0,0 +1,91 @@
+// here we define the SCHEMA for the BJJ classes or "sessions" (to avoid using typical programming words)
+// sessions are unlimited, it's every instance that a teacher holds a BJJ class
+//
+// A session's properties >>
+//
+// date: day session was held
+// scheduleSlot: pre-decided time (e.g. 10:00AM)
+// teacher: 1
+// students: 1 to 50
+// sessionType: focus, allLevels, advanced
+// topics: 1 to 3
+// techniques: 1 to 10
+// duration: 30min to 180min
+// warmup: yes/no, duration
+const mongoose = require('mongoose')
+const techniqueSubSchema = require('')
+
+const sessionSchema = mongoose.Schema(
+    {
+        // date the session was held
+        date_held: {
+            type: Date,
+            required: [true, 'Date required.'],
+        },
+        // pre-decided time (e.g. 10:00AM)
+        scheduleSlot: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Schedule'
+        },
+        // all people that attended the session
+        // should teacher be out of attendace?
+        attendance: {
+            type: Object,
+            required: [true, 'Attendance required.'],
+            properties: {
+              teacher: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: [true, 'Teacher required.'],
+                ref: 'Teacher'
+              },
+              students: {
+                type: [studentSchema],
+                required: [true, 'Students required.'],
+                ref: 'Student'
+              }
+            }
+        },
+        // type of session: focus, allLevels, advanced, etc.
+        sessionType: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'SessionType'
+        },
+        // is duration a number?? talking about minutes here
+        // should this go inside of sessionType?
+        duration: {
+            type: Number,
+            required: [true, 'Duration of session required.'],
+        },
+        // array of techniques/moves that were taught in the session
+        // techniques <=> moves ??????
+        techniques: {
+            type: [techniqueSchema],
+            ref: 'Technique',
+            required: [true, 'Techniques required.'],
+            properties: {
+                position: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+                    ref: 'Position'
+                },
+                move: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+                    ref: 'Move'
+                },
+                variation: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+                    ref: 'Variation'
+                },
+            },
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Price', priceSchema)
