869022ec6250219515e6a305cc2b2c83f6790717
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-24 15:55:48 -0400

    add belt level schema

---
+++ backend/models/techniques/beltLevelModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+const beltLevelSchema = mongoose.Schema(
+    {
+        name: {
+            type: Object,
+            required: [true, 'Name required'],
+            properties: {
+                english: {
+                    type: String,
+                    required: false,
+                },
+                portuguese: {
+                    type: String,
+                    required: false,
+                },
+                japanese: {
+                    type: Object,
+                    required: false,
+                    properties: {
+                        kanji: {
+                            type: String,
+                            required: false,
+                        },
+                        hiraganaReading: {
+                            type: String,
+                            required: false,
+                        },
+                        romajiReading: {
+                            type: String,
+                            required: false,
+                        },
+                    }
+                },
+
+            },
+
+        },
+        stripes: {
+            type: Object,
+            required: false,
+            properties: {
+                oneStripe: {
+                    type: Boolean,
+                    required: false,
+                },
+                twoStripe: {
+                    type: Boolean,
+                    required: false,
+                },
+                threeStripe: {
+                    type: Boolean,
+                    required: false,
+                },
+                fourStripe: {
+                    type: Boolean,
+                    required: false,
+                },
+            }
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('BeltLevel', beltLevelSchema)
