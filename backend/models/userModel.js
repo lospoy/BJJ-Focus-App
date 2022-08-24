3e7105b23c83287f815c23294bd130e2cb183fb1
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-24 14:58:37 -0400

    add user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,8 +4,25 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema(
     {
         name: {
-            type: String,
-            required: [true, 'Please add a name'],
+            type: Object,
+            properties: {
+                firstName: {
+                    type: String,
+                    required: [true, 'First name required'],
+                },
+                lastName: {
+                    type: String,
+                    required: [true, 'Last name required'],
+                },
+                nickName: {
+                    type: String,
+                    required: false
+                },
+                pronouns: {
+                    type: String,
+                    required: false
+                },
+            }
         },
         email: {
             type: String,
@@ -16,6 +33,16 @@ const userSchema = mongoose.Schema(
             type: String,
             required: [true, 'Please add a password'],
         },
+        student: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Student',
+        },
+        appUsage: {
+            type: [appUsageSchema],
+            required: true,
+            ref: 'AppUsage',
+        }
     },
     {
         timestamps: true,
