b9c3199014349a97f882f00c72c5c8d2623cf308
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-24 21:58:26 -0400

    add humanModel schema

---
+++ backend/models/humanModel.js
@@ -0,0 +1,68 @@
+// here we define the user SCHEMA
+const mongoose = require('mongoose')
+
+const humanSchema = mongoose.Schema(
+    {
+        name: {
+            type: Object,
+            properties: {
+                firstName: {
+                    type: String,
+                    required: [true, 'First name required'],
+                },
+                lastName: {
+                    type: String,
+                    required: [true, 'Last name required'],
+                },
+                nickName: {
+                    type: String,
+                    required: false
+                },
+                pronouns: {
+                    type: String,
+                    required: false
+                },
+            }
+        },
+        trainingHistory: {
+            type: Object,
+            required: [true, 'Specify training history. At least "brand new".' ],
+            properties: {
+                belt: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: [true, 'Belt level required.'],
+                    ref: 'Belt'
+                },
+                lastSession: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: [true, 'Last session required.'],
+                    ref: 'Session'
+                },
+                firstSession: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: [true, 'First session required.'],
+                    ref: 'Session'
+                },
+                totalTrained: {
+                    type: Object,
+                    required: [true, 'Specify total trained. Minimum 1 class.'],
+                    properties: {
+                        totalNumberOfSessions: {
+                            type: Number,
+                            required: [true, 'Total number of sessions required.'],
+                        },
+                        totalTimeTrained: {
+                            type: Number,
+                            required: [true, 'Total minutes trained required.'],
+                        }
+                    }
+                }
+            }
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Human', humanSchema)
