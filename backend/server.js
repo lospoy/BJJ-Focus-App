429240eee832fbd2f4a7e098ff6e084fccc928bd
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-25 16:11:37 -0400

    add appMetrics API route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
@@ -15,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/prices', require('./routes/priceRoutes'))
+app.use('/api/appMetrics', require('./routes/appMetricRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 // Serve frontend >> *must* be under the API routes
