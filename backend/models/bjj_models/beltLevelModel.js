d0123ead333a38227a05780b4329faea3ccb6e5e
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-26 11:13:40 -0400

    simplified beltSchema

--- backend/models/bjj_models/beltLevelModel.js
+++ backend/models/bjj_models/beltLevelModel.js
@@ -2,39 +2,9 @@ const mongoose = require('mongoose')
 
 const beltSchema = mongoose.Schema(
     {
-        name: {
-            type: Object,
+        color: {
+            type: String,
             required: [true, 'Name required'],
-            properties: {
-                english: {
-                    type: String,
-                    required: false,
-                },
-                portuguese: {
-                    type: String,
-                    required: false,
-                },
-                japanese: {
-                    type: Object,
-                    required: false,
-                    properties: {
-                        kanji: {
-                            type: String,
-                            required: false,
-                        },
-                        hiraganaReading: {
-                            type: String,
-                            required: false,
-                        },
-                        romajiReading: {
-                            type: String,
-                            required: false,
-                        },
-                    }
-                },
-
-            },
-
         },
         stripes: {
             type: Object,
