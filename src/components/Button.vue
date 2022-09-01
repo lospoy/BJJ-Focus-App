a2e2e7195169a6f2d2a867ba5e2373ec6d259acc
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-01 19:43:40 -0400

    add basic components

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button>{{ title }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        title: {
+            type: String,
+            default: 'Submit'
+        }
+    }
+}
+</script>
