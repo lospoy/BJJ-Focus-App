f0eaef6b1b541d9aba94bf5b3c0b3d5c709d6258
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-03 21:08:44 -0400

    add service

---
+++ src/services/userDataService.js
@@ -0,0 +1,19 @@
+import http from "../http-common";
+class UserDataService {
+  getAll() {
+    return http.get("/tutorials");
+  }
+  get(id) {
+    return http.get(`/tutorials/${id}`);
+  }
+  create(data) {
+    return http.post("/tutorials", data);
+  }
+  update(id, data) {
+    return http.put(`/tutorials/${id}`, data);
+  }
+  findByTitle(title) {
+    return http.get(`/tutorials?title=${title}`);
+  }
+}
+export default new UserDataService();
