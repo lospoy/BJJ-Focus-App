b99dab57f3f55ed428e1c1f927bd51d3adbda66c
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-03 21:09:00 -0400

    add components

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button type="{{ type }}" class="self-center
+    mt-6 py-2 px-20 rounded-sm self-start text-sm
+    text-white bg-at-light-orange duration-200 border-solid
+    border-2 border-transparent hover:border-at-light-orange hover:bg-at-faint-orange
+    hover:text-at-light-orange">
+    {{ title }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        title: {
+            type: String,
+            default: 'Submit'
+        },
+        type: {
+            type: String,
+            default: 'submit'
+        },
+    },
+};
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="bg-at-light-orange text-white">
+    <nav class="container py-5 px-4 flex flex-column gap-4 items-center sm:flex-row">
+        <div class="flex items-center gap-x-4">
+            <img class="w-20" src="../assets/vector/default-monochrome-white.svg" alt="">
+        </div>
+        <ul class="flex flex-1 justify-end gap-x-10"></ul>
+        <router-link class="cursor-pointer" :to="{name: 'Home'}">Home</router-link>
+        <router-link class="cursor-pointer" :to="{name: ''}">Create</router-link>
+        <router-link class="cursor-pointer" :to="{name: 'Login'}">Login</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    // Get user from store
+
+    // Setup ref to router
+
+    // Logout function
+
+    return {};
+  },
+};
+</script>
