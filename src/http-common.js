7e9006367c6d10f3b6145ebf0594fc78aa58aee0
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-04 11:49:50 -0400

    change port

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:8000/api",
   headers: {
     "Content-type": "application/json"
   }
