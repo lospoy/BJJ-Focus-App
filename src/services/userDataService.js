f994872c37077e060ebca1b676955249d56cec98
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-04 11:49:45 -0400

    update paths

--- src/services/userDataService.js
+++ src/services/userDataService.js
@@ -1,19 +1,19 @@
 import http from "../http-common";
 class UserDataService {
   getAll() {
-    return http.get("/tutorials");
+    return http.get("/users");
   }
   get(id) {
-    return http.get(`/tutorials/${id}`);
+    return http.get(`/users/${id}`);
   }
   create(data) {
-    return http.post("/tutorials", data);
+    return http.post("/users", data);
   }
   update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
+    return http.put(`/users/${id}`, data);
   }
-  findByTitle(title) {
-    return http.get(`/tutorials?title=${title}`);
+  findByName(name) {
+    return http.get(`/users?name=${name}`);
   }
 }
 export default new UserDataService();
