5b4bbd8cafea5eba532062f15f619983c9624a7d
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-04 19:14:19 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,10 @@
 import Navigation from './components/Navigation.vue'
 
 export default {
+    name: 'app',
     components: {
         Navigation,
     },
-    setup() {
-    // Create data / vars
-
-    // Check to see if user is already logged in
-
-    // If user does not exist, need to make app ready
-
-    // Runs when there is a auth state change
-    // if user is logged in, this will fire
-
-    return {};
-  },
 };
 </script>
 
