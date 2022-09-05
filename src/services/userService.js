50b92276ded23371c6f9d492fedbf4282cbc6c0c
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-05 01:06:19 -0400

    update createUser method

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,44 @@
-export async function getAllUsers() {
+// import axios from 'axios'
 
-    const response = await fetch('/api/users');
-    return await response.json();
-}
+const API_URL = 'http://localhost:8000/api/users'
+
+// Create new user
+// const createUser = async (userData) => {
+//     const response = await axios.post(API_URL, userData)
+//     return JSON.stringify(response.data)
+// }
+
+// export async function getAllUsers() {
+
+//     const response = await fetch(`${baseURL}/users`);
+//     return await response.json();
+// }
 
 export async function createUser(data) {
-    const response = await fetch(`/api/users`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({user: data})
-      })
-    return await response.json();
+
+    try {
+        const response = await fetch(`${API_URL}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+          })
+
+        if(!response.ok) {
+            throw new Error('error => response not ok')
+        } else {
+            return await response.json();
+        }
+    } catch (e) {
+        console.log(e);
+        this.setState({
+              isError: true,
+              errorMessage: e.message
+        });
+    }
 }
+
+// const userService = {
+//     createUser
+// }
+
+// export default userService
