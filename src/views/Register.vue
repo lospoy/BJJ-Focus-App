c8703663efbf99abbde16bfe538dd77433f3e113
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-04 23:18:25 -0400

    add human variable/input

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,115 +1,122 @@
 <template>
   <div class="max-w-screen-sm mx-auto px-4 py-10">
-    <!--- Error handling --->
+    <!-- Error Handling -->
     <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
-        <p class="text-red-500">{{ errorMsg }}</p>
+      <p class="text-red-500">{{ errorMsg }}</p>
     </div>
 
-    <!--- Register --->
-    <form @submit.prevent="register" class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
-        <h1 class="text-3xl text-at-light-orange mb-4 self-center">Register</h1>
+    <!-- Register -->
+    <form
+      @submit.prevent="register"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
+      <h1 class="text-3xl text-at-light-green mb-4 self-center">Register</h1>
 
-        <div class="flex flex-col mb-2">
-            <label for="email" class="mb-1 text-sm text-at-light-orange">Email</label>
-            <input
-                type="text"
-                required class="p-2 text-gray-500 focus:outline-none"
-                id="email"
-                v-model="email"
-            />
-        </div>
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green">Email</label>
+        <input
+          type="text"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="email"
+          v-model="email"
+        />
+      </div>
 
-        <div class="flex flex-col mb-2">
-            <label for="password" class="mb-1 text-sm text-at-light-orange">Password</label>
-            <input
-                type="password"
-                required class="p-2 text-gray-500 focus:outline-none"
-                id="password"
-                v-model="password"
-            />
-        </div>
+      <div class="flex flex-col mb-2">
+        <label for="password" class="mb-1 text-sm text-at-light-green">Password</label>
+        <input
+          type="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="password"
+          v-model="password"
+        />
+      </div>
 
-        <div class="flex flex-col mb-2">
-            <label for="confirmPassword" class="mb-1 text-sm text-at-light-orange">Confirm Password</label>
-            <input
-                type="password"
-                required class="p-2 text-gray-500 focus:outline-none"
-                id="confirmPassword"
-                v-model="confirmPassword"
-            />
-        </div>
+      <div class="flex flex-col mb-2">
+        <label for="confirmPassword" class="mb-1 text-sm text-at-light-green"
+          >Confirm Password</label
+        >
+        <input
+          type="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="confirmPassword"
+          v-model="confirmPassword"
+        />
+      </div>
 
-        <div class="flex flex-col mb-2">
-            <label for="human" class="mb-1 text-sm text-at-light-orange">id</label>
-            <input
-                type="text"
-                required class="p-2 text-gray-500 focus:outline-none"
-                id="human"
-                v-model="human"
-            />
-        </div>
+      <div class="flex flex-col mb-2">
+        <label for="human" class="mb-1 text-sm text-at-light-green"
+          >id</label
+        >
+        <input
+          type="text"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="human"
+          v-model="human"
+        />
+      </div>
 
-        <Button title="Register"/>
+      <Button title="Register"/>
 
-        <router-link class="text-sm mt-6 text-center" :to="{name: 'Login'}">
-            Already have an account? <span class="text-at-light-orange">Login</span>
-        </router-link>
-
+      <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">
+        Already have an account? <span class="text-at-light-green">Login</span>
+      </router-link>
     </form>
   </div>
 </template>
 
 <script>
-import { ref } from "vue"
-import { useRouter } from "vue-router"
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-
+// components import
 import Button from '../components/Button.vue'
 
 export default {
-  name: "register",
-  components: {
-    Button
-  },
+    components: {
+        Button
+    },
+name: "register",
   setup() {
-    // Create data / vars
-    const router = useRouter()
-    const email = ref(null)
-    const password = ref(null)
-    const confirmPassword = ref(null)
-    // human (human id) will be pre-populated via SMS
-    // if via email then email also pre-populated
-    const human = ref(null)
-    const errorMsg = ref(null)
+    // Tools
+    const router = useRouter();
 
+    // Variables
+    const email = ref(null);
+    const password = ref(null);
+    const confirmPassword = ref(null);
+    const human = ref(null)
+    const errorMsg = ref(null);
+
+    let newUser = {}
     // Register function
     const register = async () => {
-        const data = {
+      if (password.value === confirmPassword.value) {
+        try {
+          newUser = {
             email: email.value,
             password: password.value,
             human: human.value
+          }
+          router.push({ name: "Login" });
+        } catch (error) {
+          errorMsg.value = error.message;
+          setTimeout(() => {
+            errorMsg.value = null;
+          }, 5000);
         }
-
-        if(password.value === confirmPassword.value) {
-            try {
-                await userService.register({ data })
-                // needs to route to login with a message: "login with your new account"
-                router.push({ name: "Login" })
-            } catch (error) {
-                errorMsg.value = error.message
-                setTimeout(() => {
-                    errorMsg.value = null
-                }, 5000)
-            }
-            console.log(data)
-            return
-        } else {
-            errorMsg.value = "Passwords do not match"
-            setTimeout(() => {
-                errorMsg.value = null
-            }, 5000)
-        }
-    }
+        return
+      }
+      errorMsg.value = "Error: Passwords do not match";
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    };
+    return { email, password, confirmPassword, human, errorMsg, register, newUser };
   },
 };
 </script>
