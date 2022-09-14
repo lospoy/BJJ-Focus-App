5ed6c1dad6b3c43cfd859cff22d5d0214a660ec5
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-14 15:25:08 -0400

    add loginUser method

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,23 +1,9 @@
-// import axios from 'axios'
-
-const API_URL = 'http://localhost:8000/api/users'
-
-// Create new user
-// const createUser = async (userData) => {
-//     const response = await axios.post(API_URL, userData)
-//     return JSON.stringify(response.data)
-// }
-
-// export async function getAllUsers() {
-
-//     const response = await fetch(`${baseURL}/users`);
-//     return await response.json();
-// }
+const API_URL = 'http://localhost:5000/api/users'
 
 export async function createUser(data) {
 
     try {
-        const response = await fetch(`${API_URL}`, {
+        const response = await fetch(API_URL, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(data)
@@ -37,8 +23,82 @@ export async function createUser(data) {
     }
 }
 
+export function authHeader() {
+    // return authorization header with jwt token
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.token) {
+        return { 'Authorization': 'Bearer ' + user.token };
+    } else {
+        return {};
+    }
+}
+
+export async function loginUser(data) {
+
+    try {
+        const response = await fetch(API_URL + "/login", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+          })
+
+        if(!response.ok) {
+            throw new Error('error => response not ok')
+        } else {
+            const user = await response.json()
+            localStorage.setItem('user', JSON.stringify(user))
+        }
+
+    } catch (e) {
+        console.log(e);
+        this.setState({
+              isError: true,
+              errorMessage: e.message
+        });
+    }
+}
+
+
+
+// // making HTTP requests here
+// import axios from 'axios'
+
+// // Register user
+// const registerUser = async (userData) => {
+//     const response = await axios.post(API_URL, userData)
+
+//     // axios puts response inside an object called 'data'
+//     if(response.data) {
+//         // localStorage only accepts strings >> JSON.stringify
+//         localStorage.setItem('user', JSON.stringify(response.data))
+//     }
+
+//     return response.data
+// }
+
+// // Login user
+// const loginUser = async (userData) => {
+//     const response = await axios.post(API_URL + 'login', userData)
+
+//     // axios puts response inside an object called 'data'
+//     if(response.data) {
+//         // localStorage only accepts strings >> JSON.stringify
+//         localStorage.setItem('user', JSON.stringify(response.data))
+//     }
+
+//     return response.data
+// }
+
+// // Logout user
+// const logout = () => {
+//     localStorage.removeItem('user')
+// }
+
 // const userService = {
-//     createUser
+//     registerUser,
+//     logout,
+//     loginUser,
 // }
 
 // export default userService
