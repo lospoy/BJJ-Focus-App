4132b18e535467e4e856d84efda5cd0db823ae67
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-16 15:51:25 -0400

    config

--- vetur.config.js
+++ vetur.config.js
@@ -11,24 +11,17 @@ module.exports = {
     // **optional** default: `[{ root: './' }]`
     // support monorepos
     projects: [
-      './packages/repo2', // Shorthand for specifying only the project root location
+      './', // Shorthand for specifying only the project root location
       {
         // **required**
         // Where is your project?
         // It is relative to `vetur.config.js`.
-        root: './packages/repo1',
+        root: './',
         // **optional** default: `'package.json'`
         // Where is `package.json` in the project?
         // We use it to determine the version of vue.
         // It is relative to root property.
         package: './package.json',
-        // **optional**
-        // Where is TypeScript config file in the project?
-        // It is relative to root property.
-        tsconfig: './tsconfig.json',
-        // **optional** default: `'./.vscode/vetur/snippets'`
-        // Where is vetur custom snippets folders?
-        snippetFolder: './.vscode/vetur/snippets',
         // **optional** default: `[]`
         // Register globally Vue component glob.
         // If you set it, you can get completion by that components.
