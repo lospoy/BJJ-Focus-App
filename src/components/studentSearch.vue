02c0535e8dfc9069117eab97870ce29e49c3fe25
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-21 07:34:52 -0400

    add file

--- src/components/studentSearch.vue
+++ src/components/studentSearch.vue
@@ -1,9 +1,27 @@
+<!---
+All working until https://youtu.be/vScIosucURI?t=682
+Implementing basic search first
+--->
 <template>
-  <input type="text" name="student-search" />
+  <input
+    type="text"
+    name="student-search"
+    class="p-2 text-grey-500 focus:outline-none"
+    @keyup="search" />
 </template>
 
 <script>
+
+
 export default {
   name: "StudentSearch",
+  setup() {
+    function search(e) {
+        let search = e.target.value.trim()
+        console.log(search)
+    }
+
+    return { search }
+  }
 };
 </script>
