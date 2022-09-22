0f472507bea63c25a793ff76662381037446e1a7
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-22 13:09:02 -0400

    add file, saveSession method

---
+++ src/services/sessionService.js
@@ -0,0 +1,23 @@
+const API_URL = "http://localhost:5000/api";
+
+export async function saveSession(data) {
+  try {
+    const response = await fetch(API_URL + '/sessions', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response.json();
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
