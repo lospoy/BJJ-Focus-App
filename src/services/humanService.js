71b58fb1956dd2939d628be7cd0bf1d565c9c1b8
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-26 11:24:42 -0400

    add createHuman function

--- src/services/humanService.js
+++ src/services/humanService.js
@@ -33,3 +33,27 @@ export async function getAllHumans() {
     });
   }
 }
+
+export async function createHuman(data) {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/humans', {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + user.token },
+        body: JSON.stringify(data),
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response.json();
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
