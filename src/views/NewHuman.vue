141efd250c1511a7b73370eea19a1b80d73bc186
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-26 11:24:46 -0400

    add file

---
+++ src/views/NewHuman.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+    <!-- Error Handling -->
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- NewHuman -->
+    <form
+      @submit.prevent="newHuman"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
+      <h1 class="text-3xl text-at-light-orange mb-4 self-center">New Human</h1>
+
+      <div class="flex flex-col mb-2">
+        <label for="firstName" class="mb-1 text-sm text-at-light-orange"
+          >First Name</label
+        >
+        <input
+          type="text"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="firstName"
+          v-model="firstName"
+        />
+      </div>
+
+      <div class="flex flex-col mb-2">
+        <label for="lastName" class="mb-1 text-sm text-at-light-orange"
+          >Last Name</label
+        >
+        <input
+          type="text"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="lastName"
+          v-model="lastName"
+        />
+      </div>
+
+      <Button title="Save Human" />
+    </form>
+  </div>
+</template>nt
+
+<script>
+import { ref } from "vue";
+import { createHuman } from "../services/humanService";
+
+// components import
+import Button from "../components/Button.vue";
+
+export default {
+  name: "newHuman",
+  components: {
+    Button,
+  },
+  setup() {
+    // Variables
+    const firstName = ref(null);
+    const lastName = ref(null);
+    const errorMsg = ref(null);
+
+    // New Human function
+    const newHuman = async () => {
+        try {
+          await createHuman({
+            name: {
+                first: firstName.value,
+                last: lastName.value,
+            }
+          });
+        } catch (error) {
+          errorMsg.value = error.message;
+          setTimeout(() => {
+            errorMsg.value = null;
+          }, 5000);
+        }
+      }
+
+    return { firstName, lastName, errorMsg, newHuman };
+  },
+};
+</script>
