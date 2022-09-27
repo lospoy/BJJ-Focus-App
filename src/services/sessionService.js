b8bdc8001f2c592191963a2e2678a8e025185ee1
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-27 16:10:59 -0400

    update API_URL

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.API_URL
 
 export async function saveSession(data) {
     let user = JSON.parse(localStorage.getItem("user"))
