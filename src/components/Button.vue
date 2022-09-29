9103d61adbbbde988ae4523418b5cfe4db81de88
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-29 17:20:23 -0400

    setting up button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,11 @@
 <template>
   <button
     type="{{ type }}"
-    class="self-center mt-6 py-2 px-20 rounded-sm self-start text-sm text-white bg-at-light-orange duration-200 border-solid border-2 border-transparent hover:border-at-light-orange hover:bg-at-faint-orange hover:text-at-light-orange"
+    class=
+        "self-center mt-6 py-2 px-20 rounded-sm self-start
+        text-sm text-white bg-at-light-orange duration-200
+        border-solid border-2 border-transparent
+        hover:border-at-light-orange hover:bg-at-faint-orange hover:text-at-light-orange"
   >
     {{ title }}
   </button>
@@ -18,7 +22,7 @@ export default {
     type: {
       type: String,
       default: "submit",
-    },
+    },   
   },
 };
 </script>
