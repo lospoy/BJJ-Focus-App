2fbe4c056a5911a348401977392db3f95cf2bb52
Carlos <36555654+lospoy@users.noreply.github.com>
2022-09-30 19:46:23 -0400

    refactor

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -2,6 +2,7 @@ const API_URL = process.env.VUE_APP_ROOT_API
 
 export async function saveSession(data) {
     let user = JSON.parse(localStorage.getItem("user"))
+
   try {
     const response = await fetch(API_URL + '/sessions', {
       method: "POST",
@@ -12,7 +13,7 @@ export async function saveSession(data) {
     if (!response.ok) {
       throw new Error("error => response not ok");
     } else {
-      return await response.json();
+      return await response;
     }
   } catch (e) {
     console.log(e);
