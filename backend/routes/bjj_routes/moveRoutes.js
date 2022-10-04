2356a542b88377608d8c0340cad4308c203e5667
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 16:05:20 -0400

    delete unused route

--- backend/routes/bjj_routes/moveRoutes.js
+++ backend/routes/bjj_routes/moveRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { saveMove, getMove, updateMove, getAllMoves } = require('../../controllers/bjj_controllers/moveController')
+const { saveMove, getMove, getAllMoves } = require('../../controllers/bjj_controllers/moveController')
 const { protect } = require('../../middleware/authMiddleware')
 
 router.post('/', protect, saveMove)
 router.get('/:id', protect, getMove)
-router.put('/:id', protect, updateMove)
 router.get('/', protect, getAllMoves)
 
 module.exports = router
