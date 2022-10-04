92e91272f5adc138e47befc4f039591ed2ec22cc
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 14:33:56 -0400

    add file

---
+++ backend/routes/bjj_routes/positionRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { savePosition, getPosition, updatePosition, getAllPositions } = require('../../controllers/positionController')
+const { protect } = require('../../middleware/authMiddleware')
+
+router.post('/', protect, savePosition)
+router.get('/:id', protect, getPosition)
+router.put('/:id', protect, updatePosition)
+router.get('/', protect, getAllPositions)
+
+module.exports = router
