6c962a9341fc142833dda6a04e12759e5b22e990
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 16:05:31 -0400

    delete unused route

--- backend/routes/bjj_routes/variationRoutes.js
+++ backend/routes/bjj_routes/variationRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { saveVariation, getVariation, updateVariation, getAllVariations } = require('../../controllers/bjj_controllers/variationController')
+const { saveVariation, getVariation, getAllVariations } = require('../../controllers/bjj_controllers/variationController')
 const { protect } = require('../../middleware/authMiddleware')
 
 router.post('/', protect, saveVariation)
 router.get('/:id', protect, getVariation)
-router.put('/:id', protect, updateVariation)
 router.get('/', protect, getAllVariations)
 
 module.exports = router
