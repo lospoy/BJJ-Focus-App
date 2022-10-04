807bc863f3b20fb62d1dad2c494b10fe2fec9315
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 12:01:45 -0400

    add file

---
+++ backend/routes/techniqueRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { saveTechnique, getTechnique, updateTechnique, getAllTechniques } = require('../controllers/techniqueController')
+const { protect } = require('../middleware/authMiddleware')
+
+router.post('/', protect, saveTechnique)
+router.get('/:id', protect, getTechnique)
+router.put('/:id', protect, updateTechnique)
+router.get('/', protect, getAllTechniques)
+
+module.exports = router
