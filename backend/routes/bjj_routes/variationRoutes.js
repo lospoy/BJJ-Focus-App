41e5dfc3aa676e927af4256b8f040788b8f0e9f4
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 14:33:50 -0400

    add file

---
+++ backend/routes/bjj_routes/variationRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { saveVariation, getVariation, updateVariation, getAllVariations } = require('../../controllers/variationController')
+const { protect } = require('../../middleware/authMiddleware')
+
+router.post('/', protect, saveVariation)
+router.get('/:id', protect, getVariation)
+router.put('/:id', protect, updateVariation)
+router.get('/', protect, getAllVariations)
+
+module.exports = router
