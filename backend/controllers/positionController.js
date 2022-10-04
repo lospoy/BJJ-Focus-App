054794efbb21ea5cca0545944488291154a0a493
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 14:34:45 -0400

    delete unused vars

--- backend/controllers/positionController.js
+++ backend/controllers/positionController.js
@@ -1,7 +1,49 @@
 const asyncHandler = require('express-async-handler')
 const Position = require('../models/positionModel')
-const Move = require('../models/moveModel')
 const User = require('../models/app_models/userModel')
+
+// @desc    Save new position
+// @route   POST /api/techniques/positions
+// @access  Public
+const savePosition = asyncHandler(async (req, res) => {
+    const { name } = req.body
+
+    if (!name) {
+        res.status(400)
+        throw new Error('Please fill in all fields')
+    }
+
+    // Check if position exists
+    const positionExists = await Technique.findOne({ name: name })
+
+    if (positionExists) {
+        res.status(400)
+        throw new Error(`Technique dated ${date} already exists`)
+    }
+
+    // ************ TBD
+    // Check for user permission to save position
+    // Must be admin or teacher
+    // const isAdmin = req.user.permissions.admin === true
+    // const isTeacher = req.user.permissions.teacher === true    
+    // if(isAdmin || isTeacher) {}
+  
+    // Create position
+    const position = await Position.create({
+        name: req.body.name,
+        createdBy: req.user.id
+    })
+    if (position) {
+        res.status(201).json({
+            _id: position.id,
+            name: position.name,
+            createdBy: req.user.id
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid technique data')
+    }
+})
 
 // @desc    Get specific position data
 // @route   GET /api/techniques/positions/:id
@@ -54,6 +96,7 @@ const getAllPositions = asyncHandler(async (req, res) => {
 
 
 module.exports = {
+    savePosition,
     getPosition,
     getAllPositions,
 }
