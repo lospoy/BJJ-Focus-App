61fc29f848165c3dd37b7cf8fb19ee7df67bc057
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-05 12:54:37 -0400

    add file

---
+++ src/views/ProgressView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+    <!-- Error Handling -->
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- PROFILE -->
+    <div
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
+      <h1 class="text-3xl text-at-light-orange mb-4 self-center">Progress</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+// components import
+
+export default {
+  name: "progressView",
+  setup() {
+    // Variables
+    const errorMsg = ref(null);
+
+    return { errorMsg };
+  },
+};
+</script>
