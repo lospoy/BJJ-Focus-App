dd75aa44d5a6c2d800de942656e876df0561f4b8
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 21:05:38 -0400

    add file

---
+++ src/services/moveService.js
@@ -0,0 +1,77 @@
+// MOVE SERVICE
+
+const API_URL = process.env.VUE_APP_ROOT_API
+// import { getAuthHeader } from './userService';
+
+export async function getAllMoves() {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/techniques/moves', {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + user.token }
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response.json();
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+// GET HUMAN BY ID
+export async function getMove(id) {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/techniques/moves/' + id, {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + user.token }
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+        return await response.json()
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+export async function createMove(data) {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/techniques/moves', {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + user.token },
+        body: JSON.stringify(data),
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response;
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+
