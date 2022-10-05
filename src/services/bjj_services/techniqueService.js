6e78e29ae26e35387310172e43e08636c0913e51
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-04 22:38:48 -0400

    add file

---
+++ src/services/bjj_services/techniqueService.js
@@ -0,0 +1,77 @@
+// TECHNIQUE SERVICE
+
+const API_URL = process.env.VUE_APP_ROOT_API
+// import { getAuthHeader } from './userService';
+
+export async function getAllTechqniques() {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/techniques', {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + user.token }
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response.json();
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+// GET HUMAN BY ID
+export async function getTechnique(id) {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/techniques/id/' + id, {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + user.token }
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+        return await response.json()
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+export async function createTechnique(data) {
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    try {
+      const response = await fetch(API_URL + '/techniques', {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + user.token },
+        body: JSON.stringify(data),
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response;
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+
