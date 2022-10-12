39cd7246429772d1a8e3d3a6063512d012e0338c
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-12 18:16:54 -0400

    add file

--- backend/controllers/bjj_controllers/focusLessonController.js
+++ backend/controllers/bjj_controllers/focusLessonController.js
@@ -1,53 +1,61 @@
 const asyncHandler = require('express-async-handler')
 const FocusLesson = require('../../models/bjj_models/focusLessonModel')
 
+
 // @desc    Save new focusLesson
-// @route   POST /api/techniques/focusLessons
+// @route   POST /api/focusLessons/create
 // @access  Public
 const saveFocusLesson = asyncHandler(async (req, res) => {
-    const { topic, techniquesArray } = req.body
+    const { 
+        topic,
+        content: { techniques }
+    } = req.body
 
-    if (!topic || !techniquesArray) {
+    if (!topic || !techniques) {
         res.status(400)
-        throw new Error('Please fill topic and techniques')
+        throw new Error('Please fill in all fields')
     }
 
     // Check if focusLesson exists
-    const focusLessonExists = await FocusLesson.findOne({ topic: topic, techniquesArray: techniquesArray })
+    const focusLessonExists = await FocusLesson.findOne({ topic, content: { techniques }})
 
     if (focusLessonExists) {
         res.status(400)
-        throw new Error(`FocusLesson with topic ${topic} and techniques  ${techniquesArray} already exists`)
+        throw new Error(`FocusLesson on ${topic} with those techniques already exists`)
     }
 
     // ************ TBD
     // Check for user permission to save focusLesson
-    // Must be admin or teacher
+    // Must be admin or techniques
     // const isAdmin = req.user.permissions.admin === true
-    // const isTeacher = req.user.permissions.teacher === true    
+    // const isTeacher = req.user.permissions.techniques === true    
     // if(isAdmin || isTeacher) {}
   
     // Create focusLesson
     const focusLesson = await FocusLesson.create({
         topic: req.body.topic,
-        techniquesArray: req.body.techniquesArray,
+        content: {
+            techniques: req.body.content.techniques,
+        },
         createdBy: req.user.id
     })
     if (focusLesson) {
         res.status(201).json({
             _id: focusLesson.id,
             topic: focusLesson.topic,
-            techniquesArray: focusLesson.techniquesArray,
+            content: {
+                techniques: focusLesson.content.techniques._id,
+            },
             createdBy: req.user.id
         })
     } else {
         res.status(400)
-        throw new Error('Invalid technique data')
+        throw new Error('Invalid focusLesson data')
     }
 })
 
 // @desc    Get specific focusLesson data
-// @route   GET /api/techniques/focusLessons/:id
+// @route   GET /api/focusLessons/:id
 // @access  Private
 const getFocusLesson = asyncHandler(async (req, res) => {
     const focusLesson = await FocusLesson.findById(req.params.id)
@@ -64,11 +72,13 @@ const getFocusLesson = asyncHandler(async (req, res) => {
     }
 
     // Check for user permission to GET focusLesson data
-    // Must be admin or teacher
+    // Must be admin, techniques, or the user's focusLesson
     const isAdmin = req.user.permissions.admin
-    const isTeacher = req.user.permissions.teacher
+    const isTeacher = req.user.permissions.techniques
+    const studentIsUser = focusLesson.user ? focusLesson.user.toString() : ''
+    const isTheStudentItself = studentIsUser === req.user.id
 
-    if(isAdmin || isTeacher) {
+    if(isAdmin || isTeacher || isTheStudentItself) {
         res.status(200).json(focusLesson)
     } else {
         res.status(401)
@@ -77,15 +87,15 @@ const getFocusLesson = asyncHandler(async (req, res) => {
 })
 
 // @desc    Get all focusLessons
-// @route   GET /api/techniques/focusLessons
+// @route   GET /api/focusLessons
 // @access  Private
 const getAllFocusLessons = asyncHandler(async (req, res) => {
     const allFocusLessons = await FocusLesson.find({})
 
-    // Check for user permission to GET technique data
-    // Must be admin, teacher, or the user's technique
+    // Check for user permission to GET focusLesson data
+    // Must be admin or techniques
     const isAdmin = req.user.permissions.admin
-    const isTeacher = req.user.permissions.teacher
+    const isTeacher = req.user.permissions.techniques
 
     if(isAdmin || isTeacher) {
         res.status(200).json(allFocusLessons)
@@ -95,8 +105,51 @@ const getAllFocusLessons = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Update focusLesson data
+// @route   PUT /api/focusLessons/:id
+// @access  Private
+const updateFocusLesson = asyncHandler(async (req, res) => {
+    const focusLesson = await FocusLesson.findById(req.params.id)
+
+    if(!focusLesson) {
+        res.status(400)
+        throw new Error('FocusLesson not found')
+    }
+    
+    // Check for user
+    if(!req.user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Check for user permission to update focusLesson
+    // Must be admin, techniques, or the user's focusLesson
+    const isAdmin = req.user.permissions.admin === true
+    const isTeacher = req.user.permissions.techniques === true
+    const studentIsUser = focusLesson.user ? focusLesson.user.toString() : ''
+    const isTheStudentItself = studentIsUser === req.user.id
+    
+
+    if( isAdmin || isTeacher || isTheStudentItself ) {
+            const updatedFocusLesson = await FocusLesson.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+            })
+            res.status(200).json(updatedFocusLesson)
+    } else {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    // **************LATEST ACTION 22:45 08/30: UPDATE HUMAN
+    // update doesnt add to the property of the object, it replaces the property of the object
+    // expected is it ADDS to the property, not replace
+    // unsure if this needs to be done client side
+})
+
+
 module.exports = {
     saveFocusLesson,
     getFocusLesson,
     getAllFocusLessons,
+    updateFocusLesson,
 }
