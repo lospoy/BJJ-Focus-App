da17eccee0019949a2f43279a2ce109c70869913
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-12 21:43:50 -0400

    add file

--- src/views/Technique.vue
+++ src/views/Technique.vue
@@ -83,14 +83,89 @@
       <Button :title='buttonTitle' :color='buttonColor' />
     </form>
   </div>
+
+    <div class="max-w-screen-sm mx-auto px-4 py-10">
+        <div class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+            <h2 class="text-2xl text-at-light-orange mb-4 self-center">
+            All Techniques</h2
+            >
+            <div class="flex flex-col mb-2">
+                <label for="passList" class="mb-1 text-at-light-orange"
+                >Pass</label
+                >
+                <ul id="passList">
+                    <li v-for="(item, index) of passList" :key="index">
+                        - {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="flex flex-col mb-2">
+                <label for="entryList" class="mb-1 text-at-light-orange"
+                >Entry</label
+                >
+                <ul id="entryList">
+                    <li v-for="(item, index) of entryList" :key="index">
+                        - {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="flex flex-col mb-2">
+                <label for="escapeList" class="mb-1 text-at-light-orange"
+                >Escape</label
+                >
+                <ul id="escapeList">
+                    <li v-for="(item, index) of escapeList" :key="index">
+                        - {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="flex flex-col mb-2">
+                <label for="submissionList" class="mb-1 text-at-light-orange"
+                >Submission</label
+                >
+                <ul id="submissionList">
+                    <li v-for="(item, index) of submissionList" :key="index">
+                        - {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="flex flex-col mb-2">
+                <label for="sweepList" class="mb-1 text-at-light-orange"
+                >Sweep</label
+                >
+                <ul id="sweepList">
+                    <li v-for="(item, index) of sweepList" :key="index">
+                        - {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="flex flex-col mb-2">
+                <label for="takedownList" class="mb-1 text-at-light-orange"
+                >Takedown</label
+                >
+                <ul id="takedownList">
+                    <li v-for="(item, index) of takedownList" :key="index">
+                        - {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+        
+    </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import { createTechnique, getAllTechniques } from "../services/bjj_services/techniqueService";
-import { createPosition, getAllPositions } from "../services/bjj_services/positionService";
-import { createMove, getAllMoves } from "../services/bjj_services/moveService";
-import { createVariation, getAllVariations } from "../services/bjj_services/variationService";
+import { createPosition, getAllPositions, getPosition } from "../services/bjj_services/positionService";
+import { createMove, getAllMoves, getMove } from "../services/bjj_services/moveService";
+import { createVariation, getAllVariations, getVariation } from "../services/bjj_services/variationService";
 
 // components import
 import Button from "../components/Button.vue";
@@ -120,6 +195,66 @@ export default {
     let variationId = ref('')
     const notes = ref(null)
 
+    // All Techniques
+    const techniqueList = reactive([])
+    const passList = reactive([])
+    const entryList = reactive([])
+    const escapeList = reactive([])
+    const submissionList = reactive([])
+    const sweepList = reactive([])
+    const takedownList = reactive([])
+    
+
+    // **********************************************************************************************
+    //                                       RENDER STORED TECHNIQUES
+    // **********************************************************************************************
+    console.log(getAllTechniques())
+
+    const showAllTechniques = async() => {
+        const allTechniques = await getAllTechniques()
+        
+        for await (const technique of allTechniques) {
+            await decryptTechnique(technique.position, technique.move, technique.variation)
+        }
+    }
+    showAllTechniques()
+
+    const decryptTechnique = async(positionId, moveId, variationId) => {
+        const positionObject = await getPosition(positionId)
+        const moveObject = await getMove(moveId)
+        const variationObject = await getVariation(variationId)
+
+        if(moveObject.category.pass === true) {
+            passList.push(`${variationObject.name.english} ${moveObject.name.english} Pass from ${positionObject.name.english}`)
+        }
+
+        if(moveObject.category.entry === true) {
+           entryList.push(`${variationObject.name.english} Entry ${moveObject.name.english} from ${positionObject.name.english}`)
+        }
+
+        if(moveObject.category.escape === true) {
+           escapeList.push(`${variationObject.name.english} ${moveObject.name.english} Escape from ${positionObject.name.english}`)
+        }
+
+        if(moveObject.category.submission === true) {
+           submissionList.push(`${variationObject.name.english} ${moveObject.name.english} from ${positionObject.name.english}`)
+        }
+
+        if(moveObject.category.sweep === true) {
+           sweepList.push(`${variationObject.name.english} ${moveObject.name.english} Sweep from ${positionObject.name.english}`)
+        }
+
+        if(moveObject.category.takedown === true && positionObject.name.english === "Standing") {
+            takedownList.push(`${variationObject.name.english} ${moveObject.name.english} Takedown`)
+        } else if (moveObject.category.takedown === true) {
+            takedownList.push(`${variationObject.name.english} ${moveObject.name.english} Takedown from ${positionObject.name.english}`)
+        }
+
+
+
+    }
+    
+
     // Button success visual feedback
     let buttonColor = ref(null) 
     let buttonTitle = ref("Save Technique")
@@ -142,7 +277,7 @@ export default {
     // **********************************************************************************************
     const newPosition = async () => {
         const allPositions = await getAllPositions()
-        // Check if position already exists
+
         const foundPosition = allPositions.filter(x =>
             x.name.english.toLowerCase() === position.value.toLowerCase())[0]
 
@@ -298,6 +433,8 @@ export default {
     return {
         position, positionId, move, moveId, moveCategory, variation, variationId, notes,
         passOption, entryOption, escapeOption, submissionOption, sweepOption, takedownOption,
+        techniqueList, showAllTechniques, getMove, getPosition, getVariation, decryptTechnique,
+        passList, entryList, escapeList, submissionList, sweepList, takedownList,
         errorMsg, buttonColor, buttonTitle, buttonSuccess,
         newPosition, newMove, newVariation, newTechnique
     };
