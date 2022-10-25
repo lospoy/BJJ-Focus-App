0570eb4328d8b2ceeff95903e21dbc76024e3b3f
Carlos <36555654+lospoy@users.noreply.github.com>
2022-10-25 15:44:28 -0400

    working current and next topic

--- src/views/ProgressView.vue
+++ src/views/ProgressView.vue
@@ -9,7 +9,8 @@
     <div
       class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
     >
-      <h1 class="text-3xl text-at-light-orange mb-4 self-center">Progress</h1>
+      <h1 class="text-4xl text-at-light-orange mb-1 self-center">{{ currentTopic }}</h1>
+      <h2 class="text-sm text-at-light-orange mb-1 self-center">Next Topic: {{ nextTopic }}</h2>
 
     </div>
   </div>
@@ -25,8 +26,57 @@ export default {
   setup() {
     // Variables
     const errorMsg = ref(null);
+    const currentTopic = ref(null);
+    const nextTopic = ref(null)
 
-    return { errorMsg };
+    // One topic per week
+    const weeklyTopicList = [
+        "Back Control",     // semana 1
+        "Half Guard",       // semana 2
+        "Side Control",     // semana 3
+        "Closed Guard",     // ...
+        "Mount",
+        "De la Riva",
+        "Open Guard & North-South", // estamos aqui semana 7
+        "Turtle and Takedowns"  
+    ]
+
+    // Cambiar el 6 por una variable
+    // Cambio basado en Date() o algo asi
+    // Al final del array, vuelve al principio
+
+    // Sistema tiene que ser consciente del dia actual
+    // Evento al cambio de fecha -> checkeo una vez, si el dia es X -> cambio
+    // Hay que almacenar el numero de la semana de lo que llevamos de curso
+
+    // Crear cuentaSemana en base de datos
+    // Cada vez que pasa una semana cuentaSemana+1
+    // get cuentaSemana y pasarlo en la funcion de abajo
+
+    // Set a start date
+    // count how many weeks from start date
+    // pass in variable below
+
+    // Counts how many weeks have passed since start date and "today"
+    const countWeeksPassed = _ => {
+         // Day the classes started
+        const startDate = new Date("September 12, 2022 18:00:00")
+        // Today's date
+        const endDate = new Date()
+
+        const diffInMs = new Date(endDate) - new Date(startDate)
+        const diffInWeeks = diffInMs / (1000 * 60 * 60 * 24 ) / 7;
+
+        return diffInWeeks
+    }
+
+    const weeksFromStartToCurrent = Math.floor(countWeeksPassed() % 8)
+    const weeksFromStartToNext = Math.floor(countWeeksPassed() +1 % 8)
+
+    currentTopic.value = weeklyTopicList[weeksFromStartToCurrent]
+    nextTopic.value = weeklyTopicList[weeksFromStartToNext]
+
+    return { errorMsg, weeklyTopicList, currentTopic, nextTopic, countWeeksPassed };
   },
 };
 </script>
