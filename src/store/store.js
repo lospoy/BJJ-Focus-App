1ad3e6c8bec2f3d4ffd83f1830a20fce2965435a
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-02 17:30:41 -0400

    add isLoggedIn state and checkLogin function

--- src/store/store.js
+++ src/store/store.js
@@ -4,14 +4,19 @@ import { reactive } from "vue";
 
 const state = reactive({
   user: null,
+  isLoggedIn: null
 });
 
 const methods = {
   setUser(payload) {
-    state.user = payload ? payload : null;
+    state.user = payload ? payload : null,
+    state.isLoggedIn = state.user ? true : false
   },
   getUser() {
     return JSON.stringify(state.user)
+  },
+  checkLogin() {
+    return state.isLoggedIn
   }
 };
 
