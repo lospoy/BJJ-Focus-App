327d97f0067d9c5fb648aa11867c9964af2eef12
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-07 15:44:28 -0500

    80vh hotfix

--- src/views/PrivateClass.vue
+++ src/views/PrivateClass.vue
@@ -11,7 +11,7 @@
         <div
             class="flex justify-center calendly-inline-widget"
             data-url="https://calendly.com/lospoy/private-class?&hide_gdpr_banner=1&background_color=f7f7f7&text_color=3d4250&primary_color=d85a3b"
-            style="display:flex;height:55vh;min-width:97%;align-items: stretch;"
+            style="display:flex;height:80vh;min-width:97%;align-items: stretch;"
             >
         </div>
     </div>
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 import { getHuman } from "../services/humanService"
 import { getAllSessions } from "../services/sessionService"
 import store from "../store/store"
