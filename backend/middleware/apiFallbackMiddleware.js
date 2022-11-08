77dd2cc903888ebd4f74bc436c48fb80be38ae38
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-08 15:21:27 -0500

    change function to fat arrow syntax

--- backend/middleware/apiFallbackMiddleware.js
+++ backend/middleware/apiFallbackMiddleware.js
@@ -1,15 +1,15 @@
 const history = require('connect-history-api-fallback');
-const express = require('express');
 
-const apiRedirect = express();
-apiRedirect.use(history({
-    rewrites: [
-        {
-            from: /.*/,
-            to: '/index.html'
-        }
-    ],
-    logger: console.log.bind(console)
-}));
+const apiRedirect = _ => {
+    history({
+        rewrites: [
+            {
+                from: /.*/,
+                to: '/index.html'
+            }
+        ],
+        logger: console.log.bind(console)
+    })
+};
 
 module.exports = { apiRedirect }
