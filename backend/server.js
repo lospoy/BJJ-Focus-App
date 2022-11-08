a90a8aa8d67b86e1e0a82f0febcd73d25a1367ab
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-08 13:00:32 -0500

    added apiRedirect

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 8000
-
+const { apiRedirect } = require("./middleware/apiFallbackMiddleware")
 
 
 connectDB()
@@ -21,6 +21,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors());
+app.use(apiRedirect)
 
 app.use('/api/appMetrics', require('./routes/app_routes/appMetricRoutes'))
 app.use('/api/users', require('./routes/app_routes/userRoutes'))
