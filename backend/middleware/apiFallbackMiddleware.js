42d1f43987c5b0eff7fd767a2a9732721dd399cc
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-08 12:35:37 -0500

    add rewrite (any to /index)

--- backend/middleware/apiFallbackMiddleware.js
+++ backend/middleware/apiFallbackMiddleware.js
@@ -1,6 +1,9 @@
 const history = require('connect-history-api-fallback');
-
 const express = require('express');
 
 const app = express();
-app.use(history());
+app.use(history({
+    rewrites: [
+        { from: /.*/, to: '/index.html' }
+    ]
+}));
