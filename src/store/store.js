a4420e88271ca4d5ef8b1be23406bf60e41b0c98
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-09 17:33:43 -0500

    add student setters and getters

--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,10 @@ const state = reactive({
   isLoggedIn: null
 });
 
+const student = reactive({
+  data: null
+});
+
 const methods = {
   setUser(payload) {
     state.user = payload ? payload : null,
@@ -17,10 +21,17 @@ const methods = {
   },
   checkLogin() {
     return state.isLoggedIn
+  },
+  setStudent(payload) {
+    student.data = payload ? payload : null
+  },
+  getStudent() {
+    return JSON.parse(JSON.stringify(student.data))
   }
 };
 
 export default {
   state,
+  student,
   methods,
 };
