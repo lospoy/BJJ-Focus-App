e0f30fdc7778cdfa6efa543b88dc4084fc1966b6
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-10 16:28:58 -0500

    refactoring, add skeleton loading

--- src/components/StudentStats.vue
+++ src/components/StudentStats.vue
@@ -4,8 +4,15 @@
       <div class="rounded-md bg-at-light-orange mb-2 self-center">
         <span class="flex text-m text-white px-24">Stats</span>
       </div>
-      <div class="pl-4 px-6">
+      <div class="pl-4 px-6 animate-pulse" v-if="skeleton">
             <ul class="list-inside space-y-1 justify-center">
+                <li class="text-l text-dark-grey uppercase">Latest Session: {{ latestSession }}</li>
+                <li class="text-l text-dark-grey uppercase">First Session: {{ firstSession }}</li>
+                <li class="text-l text-dark-grey uppercase">Focus Training: {{ totalTrained }}</li>
+            </ul>
+      </div>
+      <div class="pl-4 px-6">
+            <ul class="list-inside space-y-1 justify-center" v-if="stats">
                 <li class="text-l text-dark-grey uppercase">Latest Session: {{ latestSession }}</li>
                 <li class="text-l text-dark-grey uppercase">First Session: {{ firstSession }}</li>
                 <li class="text-l text-dark-grey uppercase">Focus Training: {{ totalTrained }}</li>
@@ -16,7 +23,6 @@
 
 <script>
 import { ref } from "vue";
-import { getAllSessions } from "../services/sessionService"
 import store from "../store/store"
 
 export default {
@@ -24,51 +30,57 @@ export default {
   setup() {
     // Variables
     const errorMsg = ref(null);
-    const user = JSON.parse(store.methods.getUser());
-    const focusSessions = ref(null)
     const totalTrained = ref(null)
     const firstSession = ref(null)
     const latestSession = ref(null)
+    const skeleton = ref(null)
+    const stats = ref(null)
 
-    //  **************  USER'S TRAINING DATA ************** 
-    //
-    const getTrainingData = async() => {
-        const allSessions = await getAllSessions()
+    const skeletonService = _ => {
+      skeleton.value = true
+      stats.value = false
 
-        const sessionsAttendedByUser = allSessions.filter(session => JSON.stringify(session.who.students).includes(user.human))
-        focusSessions.value = sessionsAttendedByUser.length
-
-        const firstSessionAttendedByUser = sessionsAttendedByUser[0]
-        firstSession.value = new Date(firstSessionAttendedByUser.when.date).toLocaleDateString()
-
-        const latestSessionAttendedByUser = sessionsAttendedByUser[sessionsAttendedByUser.length-1]
-        const diffInDays = new Date(latestSessionAttendedByUser.when.date) - new Date(firstSessionAttendedByUser.when.date) // in miliseconds
-        latestSession.value = `${Math.floor(diffInDays / (1000 * 60 * 60 * 24 ))} days ago`
-
-        const totalTrainedByUser = new Date() - new Date(firstSessionAttendedByUser.when.date)  // in miliseconds
-        const diffInWeeks = totalTrainedByUser / (1000 * 60 * 60 * 24 ) / 7
-        if (diffInWeeks < 1) {
-            totalTrained.value = "Just Started!💪"
-        } else if (diffInWeeks >= 1 && diffInWeeks < 8) {
-            totalTrained.value = `${Math.floor(diffInWeeks)} weeks🔥`
-        } else if (diffInWeeks >= 8 && diffInWeeks < 52) {
-            totalTrained.value = `${Math.floor(diffInWeeks/4)} months⚡`
-        } else {
-            totalTrained.value = `${(diffInWeeks/4/12).toFixed(2)} years⚡`
-        }
-
-        store.methods.setStudent({
-          focusSessions: focusSessions.value,
-          firstSession: firstSession.value,
-          latestSession: latestSessionAttendedByUser.when.date,
-          weeksTrained: diffInWeeks
-        })
+      setTimeout(() => {
+        skeleton.value = false
+        stats.value = true
+      }, 2300);
     }
-    getTrainingData()
+    skeletonService()
+
+    const displayStudentData = async() => {
+      latestSession.value = '...'
+      firstSession.value = '...'
+      totalTrained.value = '...'
+
+      setTimeout(() => {
+        const training = store.methods.getStudent().training
+
+        // LATEST SESSION
+        latestSession.value = `${training.daysSinceLatestSession} days ago`
+
+        // FIRST SESSION
+        firstSession.value = new Date(training.firstSession).toLocaleDateString()
+
+        // FOCUS TRAINING
+        const weeksTrained = training.weeksTrained
+        if (weeksTrained < 1) {
+            totalTrained.value = "Just Started!💪"
+        } else if (weeksTrained >= 1 && weeksTrained < 8) {
+            totalTrained.value = `${Math.floor(weeksTrained)} weeks🔥`
+        } else if (weeksTrained >= 8 && weeksTrained < 52) {
+            totalTrained.value = `${Math.floor(weeksTrained/4)} months⚡`
+        } else {
+            totalTrained.value = `${(weeksTrained/4/12).toFixed(2)} years⚡`
+        }
+        
+      }, 2300);
+    }
+    displayStudentData()
 
     return {
         errorMsg,
-        focusSessions, totalTrained, firstSession, latestSession,
+        skeleton, stats,
+        totalTrained, firstSession, latestSession,
     };
   },
 };
