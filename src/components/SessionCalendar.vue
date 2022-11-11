6a3b8451cf3efae1c31915f4e81ceadca39762c8
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-11 00:20:00 -0500

    working attended and unnatended past sessions

--- src/components/SessionCalendar.vue
+++ src/components/SessionCalendar.vue
@@ -6,43 +6,86 @@
       </div>
       <div class="">
       <v-calendar
+        class="animate-pulse"
+        is-expanded
+        v-if="skeleton"
+      />
+      <v-calendar
         is-expanded
         :attributes="calendarAttrs"
+        v-if="calendar"
       />
       </div>
     </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import store from "../store/store"
-
 
 export default {
   name: "SessionCalendar",
   setup() {
     // Variables
     const errorMsg = ref(null);
+    const skeleton = ref(null)  // v-if
+    const calendar = ref(null) // v-if
+    const delay = 3000  // ms delay to sync the skeletonService and displayStudentData setTimeouts
+    
+    const skeletonService = _ => {
+      skeleton.value = true
+      calendar.value = false
+
+      setTimeout(() => {
+        skeleton.value = false
+        calendar.value = true
+      }, delay);
+    }
+    skeletonService()
 
     // **************  CALENDAR ************** 
     const date = ref(null)
-    const calendarAttrs = [  // Attributes are supplied as an array
-      {
-        key: 'today',
-        highlight: true,
-        dates: new Date(),
-      },
-      {
-        highlight: {
-          color: 'gray',
-          fillMode: 'outline'
+    const calendarAttrs = ref(null)
+
+    setTimeout(() => {
+      const training = store.methods.getStudent().training
+      const unattendedSessions = training.unattendedSessions.map(session => session.when.date)
+      const attendedSessions = training.attendedSessions.map(session => session.when.date)
+
+      calendarAttrs.value = [  // Attributes are supplied as an array
+        { // today
+          key: 'today',
+          dot: 'yellow',
+          dates: new Date(),
         },
-        dates: firstSession.value,
-      },
-    ]
+        { // sessions held but unnattended by user
+          highlight: {
+            style: {
+              backgroundColor: 'rgba(231, 201, 59, .3)',
+              borderColor : 'rgba(231, 201, 59, 0)'
+            },
+            fillMode: 'outline'
+          },
+          dates: unattendedSessions,
+        },
+        { // sessions attended by user
+          highlight: {
+            style: {
+              backgroundColor: '#E7C93B',
+              borderColor : '#E7C93B'
+            },
+            fillMode: 'outline'
+          },
+          dates: attendedSessions,
+        },
+      ]
+
+      console.log(unattendedSessions)
+    }, delay);
 
     return {
-        errorMsg, date, calendarAttrs
+        errorMsg, date, calendarAttrs,
+        skeleton, calendar
     };
   },
 };
