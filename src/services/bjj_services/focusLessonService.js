088ee536673854212cbab8284c1cc0b3f970915f
Carlos <36555654+lospoy@users.noreply.github.com>
2022-11-11 11:41:24 -0500

    add file

---
+++ src/services/bjj_services/focusLessonService.js
@@ -0,0 +1,79 @@
+// FOCUS LESSON SERVICE
+
+const API_URL = process.env.VUE_APP_ROOT_API
+// import { getAuthHeader } from './userService';
+
+// GET ALL
+export async function getAllFocusLessons() {
+    let user = JSON.parse(localStorage.getItem("BJJFocusUser"))
+
+    try {
+      const response = await fetch(API_URL + '/focusLessons', {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + user.token }
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response.json();
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+// GET BY ID
+export async function getFocusLesson(id) {
+    let user = JSON.parse(localStorage.getItem("BJJFocusUser"))
+
+    try {
+      const response = await fetch(API_URL + '/focusLessons' + id, {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + user.token }
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+        return await response.json()
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+// CREATE
+export async function createFocusLesson(data) {
+    let user = JSON.parse(localStorage.getItem("BJJFocusUser"))
+
+    try {
+      const response = await fetch(API_URL + '/focusLessons/create', {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + user.token },
+        body: JSON.stringify(data),
+      });
+
+    if (!response.ok) {
+      throw new Error("error => response not ok");
+    } else {
+      return await response;
+    }
+  } catch (e) {
+    console.log(e);
+    this.setState({
+      isError: true,
+      errorMessage: e.message,
+    });
+  }
+}
+
+
