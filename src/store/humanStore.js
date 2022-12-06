110bfdb90a09e4caa124fbf40c7abbe87f79cac6
Carlos <36555654+lospoy@users.noreply.github.com>
2022-12-06 00:10:22 -0500

    cleanup code

--- src/store/humanStore.js
+++ src/store/humanStore.js
@@ -2,21 +2,19 @@
 import { getHuman } from "../services/humanService";
 import { getAllSessions } from "../services/sessionService";
 
-const methods = {
+const methods = { // [ "Fizz Mcbuzz", "Pepe papa", ]
   async getStudentName(id) {
     const human = await getHuman(id)
     const humanName = human.name.first + " " + human.name.last
     return humanName
   },
+
   async getStudentLastAttendedSession(id) {
     const allSessions = await getAllSessions()
     const sessionsAttendedByUser = allSessions.filter(session => JSON.stringify(session.who.students).includes(id))
     const latestSessionAttended = sessionsAttendedByUser[sessionsAttendedByUser.length-1]
     return latestSessionAttended
   },
-  async getStudentLastAttendedSessionDate(id) {
-    getStudentLastAttendedSession(id).then(res => res.when.date)
-  }
 
 }
 
