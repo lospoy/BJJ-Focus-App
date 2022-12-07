848a49334ce60dfa300e76826ccaa4ae596952b3
Carlos <36555654+lospoy@users.noreply.github.com>
2022-12-07 16:29:36 -0500

    log testing

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.use('/api/focusLessons', require('./routes/bjj_routes/focusLessonRoutes'))
 // Serve frontend >> *must* be under the API routes
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('/', (req, res) => res.send('TEsting HI PLEASE FIND ME'))
     // all subdomain requests sent to index.html
     app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, '../', 'dist', 'index.html')))
 } else {
