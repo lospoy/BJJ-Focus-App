6ae10368c91db3a89fdbd87a2b09c8c6504dfc96
Carlos <36555654+lospoy@users.noreply.github.com>
2022-12-12 11:41:45 -0500

    add file

---
+++ src/views/Student.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="max-w-screen-sm mx-auto px-4 py-5">
+    <!-- Error Handling -->
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- STATS (components) -->
+    <StudentStats :title='studentTitle'/>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { getHuman } from "../services/humanService"
+import store from "../store/store"
+import StudentStats from "../components/StudentStats.vue"
+import { setTrainingData } from "../store/trainingData"
+
+export default {
+  name: "student",
+  components: {
+    StudentStats
+  },
+  setup() {
+    // Variables
+    const errorMsg = ref(null);
+    let studentTitle = ref("Student Stats")
+    
+    return {
+        errorMsg,
+        studentTitle
+    };
+  },
+};
+</script>
