141f5c8b3f522b1ce146c28e2c36ee1b7a316b40
Carlos <36555654+lospoy@users.noreply.github.com>
2022-12-14 00:02:20 -0500

    refactor

--- src/store/trainingData.js
+++ src/store/trainingData.js
@@ -1,35 +1,34 @@
-// Gets user's training data from server and sets it in the store
+// Gets a student's (human) training data from server and sets it in the store
 // Used in /views/StudentStats.vue
 import { getAllSessions } from "../services/sessionService";
 import store from './store';
 
-export async function setTrainingData() {
-  const user = await store.methods.getUser()
+export async function setTrainingData(humanId) {
   const allSessions = await getAllSessions()
 
-  const sessionsAttendedByUser = allSessions.filter(session => JSON.stringify(session.who.students).includes(user.human))
-  const sessionsUnattendedByUser = allSessions.filter(session => !JSON.stringify(session.who.students).includes(user.human))
+  const sessionsAttended = allSessions.filter(session => JSON.stringify(session.who.students).includes(humanId))
+  const sessionsUnattended = allSessions.filter(session => !JSON.stringify(session.who.students).includes(humanId))
   
-  const focusSessions = sessionsAttendedByUser.length
+  const focusSessions = sessionsAttended.length
 
-  const firstSessionAttendedByUser = sessionsAttendedByUser[0]
-  const firstSession = new Date(firstSessionAttendedByUser.when.date)
+  const firstSessionAttended = sessionsAttended[0]
+  const firstSession = new Date(firstSessionAttended.when.date)
 
-  const latestSessionAttendedByUser = sessionsAttendedByUser[sessionsAttendedByUser.length-1]
-  const diffInMs = new Date() - new Date(latestSessionAttendedByUser.when.date) // in miliseconds
+  const latestSessionAttended = sessionsAttended[sessionsAttended.length-1]
+  const diffInMs = new Date() - new Date(latestSessionAttended.when.date) // in miliseconds
   const daysSinceLatestSession = Math.floor(diffInMs / (1000 * 60 * 60 * 24 ))
 
-  const totalTrainedInMsInMs = new Date(latestSessionAttendedByUser.when.date) - new Date(firstSessionAttendedByUser.when.date)  // in miliseconds
+  const totalTrainedInMsInMs = new Date(latestSessionAttended.when.date) - new Date(firstSessionAttended.when.date)  // in miliseconds
   const weeksTrained = Math.round(totalTrainedInMsInMs / (1000 * 60 * 60 * 24 ) / 7)
 
   store.methods.setTraining({
     allSessions: allSessions,
-    unattendedSessions: sessionsUnattendedByUser,
+    unattendedSessions: sessionsUnattended,
     focusSessions: focusSessions,
     firstSession: firstSession,
-    latestSession: latestSessionAttendedByUser.when.date,
+    latestSession: latestSessionAttended.when.date,
     weeksTrained: weeksTrained,
     daysSinceLatestSession: daysSinceLatestSession,
-    attendedSessions: sessionsAttendedByUser
+    attendedSessions: sessionsAttended
   })
 }
