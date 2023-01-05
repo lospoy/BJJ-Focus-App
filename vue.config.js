89ae9fccbb8aef46e84ebaf63514c352f6b2cb76
Carlos <36555654+lospoy@users.noreply.github.com>
2023-01-05 17:30:02 -0500

    cleanup config

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: { port: 8000 },
+});
+
+module.exports = {
+  
+    pages: {
+      index: {
+        // entry for the page
+        entry: 'src/main.js',
+        title: 'BJJ Focus',
+      },
+    }
+  }
+
+module.exports = defineConfig({
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  }
+})
