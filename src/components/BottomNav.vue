55496a46088a23bccc565038aa7e288f9cdec4c8
Carlos <36555654+lospoy@users.noreply.github.com>
2023-01-06 15:47:34 -0500

    add student and admin routes/navbars

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,76 +1,105 @@
 <template>
-  <v-bottom-navigation>
-    
-    <router-link v-if="user && isAdmin" class="cursor-pointer grid" :to="{ name: 'NewHuman' }">
-      <v-btn value="recent">
-        <v-icon>mdi-history</v-icon>
-        Recent
+
+    <!-- STUDENT NAV -->
+    <v-bottom-navigation density="compact" v-if="isStudent">
+
+      <v-btn value="recent" to="/">
+        <v-icon>mdi-home</v-icon>
       </v-btn>
-    </router-link>
 
-    <v-btn value="favorites">
-      <v-icon>mdi-heart</v-icon>
+      <v-btn value="balanceBoard" :to="{ name: 'BalanceBoard' }">
+        <v-icon>mdi-chart-box-outline</v-icon>
+      </v-btn>
 
-      Favorites
-    </v-btn>
+      <v-btn value="privates" :to="{ name: 'PrivateClass' }">
+        <v-icon>mdi-brain</v-icon>
+      </v-btn>
 
-    <v-btn value="nearby">
-      <v-icon>mdi-map-marker</v-icon>
+      <v-btn value="sessionHistory">
+        <v-icon>mdi-calendar-multiselect</v-icon>
+      </v-btn>
+      
+      <v-btn value="settings">
+        <v-icon>mdi-nut</v-icon>
+      </v-btn>
 
-      Nearby
-    </v-btn>
-  </v-bottom-navigation>
+    </v-bottom-navigation>
+
+    <!-- ADMIN NAV -->
+    <v-bottom-navigation density="compact" v-if="isAdmin">
+
+      <v-btn value="studentList" :to="{ name: 'StudentList' }">
+        <v-icon>mdi-account-details</v-icon>
+      </v-btn>
+
+      <v-btn value="studentProgress" :to="{ name: 'ProgressView' }">
+        <v-icon>mdi-account-search</v-icon>
+      </v-btn>
+
+      <v-btn value="session" :to="{ name: 'Session' }">
+        <v-icon>mdi-calendar-plus</v-icon>
+      </v-btn>
+      
+      <v-btn value="human" :to="{ name: 'Human' }">
+        <v-icon>mdi-human</v-icon>
+      </v-btn>
+
+      <v-btn value="technique" :to="{ name: 'Technique' }">
+        <v-icon>mdi-chess-pawn</v-icon>
+      </v-btn>
+
+    </v-bottom-navigation>
+
+    <router-view></router-view>
 </template>
 
 <script>
 import { logoutUser } from "../services/userService";
 import { useRouter } from "vue-router";
 import store from "../store/store"
-import { getHuman } from "../services/humanService"
-import { inject, ref } from "vue"        // required for the emitter (EventBus)
+import { inject, ref, onMounted } from "vue"        // required for the emitter (EventBus)
 
 export default {
-    setup() {
-    // VARIABLES
-    const router = useRouter();
-    const humanName = ref(null)
-    const isAdmin = ref(null)
-    const isStudent = ref(null)
-    let user = JSON.parse(localStorage.getItem("BJJFocusUser"))
-
-    // On-login the app does some checks
-    if(user) {
-        // Gets currently logged human's name and assigns to the variable
-        const getHumanName = async () => {
-            const res = await getHuman(user.human)
-            humanName.value = `${res.name.first} ${res.name.last}`
-        }
-        getHumanName()
-        // If user logs in, checks for their role to display relevant routes
-        isAdmin.value = user.role.admin
-        isStudent.value = user.role.student
+  name: "BottomNav",
+  props: {
+    role: {
+      type: String,
+      required: true,
     }
+  },
+  setup(props) {
+  // VARIABLES
+  const router = useRouter();
+  const isAdmin = ref(null)
+  const isStudent = ref(null)
+  let user = JSON.parse(localStorage.getItem("BJJFocusUser"))
+  // Emitter (EventBus) this section emits an event that can be listened to globally
+  const emitter = inject('emitter')
+  const emitLogout = _ => {
+      emitter.emit('userHasLoggedOut', true)
+  }
+    // Logout function
+    const logout = async () => {
+      logoutUser();
+      store.methods.setUser(user)
+      
+      setTimeout(() => {
+          emitLogout()
+      }, 1);
+      setTimeout(() => {
+          router.push({ name: "Login" });
+      }, 700);
+    };
 
-    // Emitter (EventBus) this section emits an event that can be listened to globally
-    const emitter = inject('emitter')
-    const emitLogout = _ => {
-        emitter.emit('userHasLoggedOut', true)
-    }
+    onMounted(() => {
+      if(props.role === 'admin') isAdmin.value = true
+      if(props.role === 'student') isStudent.value = true
+    })
 
-      // Logout function
-      const logout = async () => {
-        logoutUser();
-        store.methods.setUser(user)
-        
-        setTimeout(() => {
-            emitLogout()
-        }, 1);
-        setTimeout(() => {
-            router.push({ name: "Login" });
-        }, 700);
-      };    
-      return { logout, store, user, isAdmin, isStudent, emitLogout, humanName };
-    },
+    return {
+      logout, store, user, isAdmin, isStudent, emitLogout
+    };
+  },
 
 };
 </script>
