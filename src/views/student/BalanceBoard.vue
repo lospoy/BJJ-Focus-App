b38289f2f765bed439bb79bd63726a81b37003a9
Carlos <36555654+lospoy@users.noreply.github.com>
2023-01-10 03:18:50 -0500

    accent color

--- src/views/student/BalanceBoard.vue
+++ src/views/student/BalanceBoard.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="max-w-screen-sm mx-auto py-5 px-1">
+  <div class="max-w-screen-sm mx-auto py-5 px-3">
     <!-- Error Handling -->
     <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
 
     <!-- TEXT ABOVE CHART  -->
-    <div class="p-5 bg-light-grey rounded-md flex flex-col justify-center mb-2">
+    <div class="px-5 pt-6 pb-12 bg-dark-grey rounded-md flex flex-col justify-center -mb-4">
       <div class="flex flex-col pl-4 px-6 w-full">
             <ul class="list-inside space-y-1 self-center" >
-              <li class="text-2xl text-dark-grey uppercase text-center">A BALANCED GAME</li>
+              <li class="text-xl text-light-grey uppercase text-center">A BALANCED GAME</li>
             </ul>
             <div class="flex flex-col mt-2 w-full">
               <ul id="techniqueList" class="space-y-1 ml-4">
-                  <li class="text-dark-grey px-2">
+                  <li class="text-light-grey text-xs px-2">
                     Tu date cuenta que cuando trabajas en cosas por igual pues no vas a ser un matao en nada
                   </li>
               </ul>
