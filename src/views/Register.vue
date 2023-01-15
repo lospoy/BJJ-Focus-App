35f1a28fd8606f5d48efb9cd618dfe28dee2916f
Carlos <36555654+lospoy@users.noreply.github.com>
2023-01-15 12:35:43 -0500

    style update

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,55 +8,55 @@
     <!-- Register -->
     <form
       @submit.prevent="register"
-      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+      class="p-8 flex flex-col bg-dark-grey rounded-md shadow-lg"
     >
-      <h1 class="text-3xl text-at-light-orange mb-4 self-center">Register</h1>
+      <h1 class="text-3xl text-light-grey mb-4 self-center">Register</h1>
 
       <div class="flex flex-col mb-2">
-        <label for="email" class="mb-1 text-sm text-at-light-orange"
+        <label for="email" class="mb-1 text-sm text-light-grey"
           >Email</label
         >
         <input
           type="text"
           required
-          class="p-2 text-gray-500 focus:outline-none"
+          class="p-2 rounded-sm bg-med-grey text-light-grey focus:outline-none"
           id="email"
           v-model="email"
         />
       </div>
 
       <div class="flex flex-col mb-2">
-        <label for="password" class="mb-1 text-sm text-at-light-orange"
+        <label for="password" class="mb-1 text-sm text-light-grey"
           >Password</label
         >
         <input
           type="password"
           required
-          class="p-2 text-gray-500 focus:outline-none"
+          class="p-2 rounded-sm bg-med-grey text-light-grey focus:outline-none"
           id="password"
           v-model="password"
         />
       </div>
 
       <div class="flex flex-col mb-2">
-        <label for="confirmPassword" class="mb-1 text-sm text-at-light-orange"
+        <label for="confirmPassword" class="mb-1 text-sm text-light-grey"
           >Confirm Password</label
         >
         <input
           type="password"
           required
-          class="p-2 text-gray-500 focus:outline-none"
+          class="p-2 rounded-sm bg-med-grey text-light-grey focus:outline-none"
           id="confirmPassword"
           v-model="confirmPassword"
         />
       </div>
 
       <div class="flex flex-col mb-2">
-        <label for="human" class="mb-1 text-sm text-at-light-orange">id</label>
+        <label for="human" class="mb-1 text-sm text-light-grey">id</label>
         <input
           type="text"
           required
-          class="p-2 text-gray-500 focus:outline-none"
+          class="p-2 rounded-sm bg-med-grey text-light-grey focus:outline-none"
           id="human"
           v-model="human"
         />
@@ -64,8 +64,8 @@
 
       <Button title="Register" />
 
-      <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">
-        Already have an account? <span class="text-at-light-orange">Login</span>
+      <router-link class="text-sm mt-6 text-center text-light-grey" :to="{ name: 'Login' }">
+        Already have an account? <span class="text-gold">Login</span>
       </router-link>
     </form>
   </div>
