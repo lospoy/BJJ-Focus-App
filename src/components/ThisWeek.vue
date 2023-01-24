314e7ae3d7b8c41996c4a3b1b7c3512467032698
Carlos <36555654+lospoy@users.noreply.github.com>
2023-01-24 15:26:16 -0500

    add skills percentages for teachers

--- src/components/ThisWeek.vue
+++ src/components/ThisWeek.vue
@@ -1,38 +1,58 @@
 <template>
-    <div class="flex flex-col bg-dark-grey mb-4 p-3 mt-2">
-      <!-- content -->
-      <div class="flex flex-col px-4">
-        <h2 class="text-sm -mb-2 mt-2 text-med-grey2">This week's topic is</h2>
-        <h1 class="text-4xl text-white uppercase">{{ currentTopic }}</h1>
+<div class="flex flex-col bg-dark-grey mb-4 p-3 mt-2">
+  <div class="flex flex-col px-4">
 
+    <!-- TOPIC -->
+    <h2 class="text-sm -mb-2 mt-2 text-med-grey2">This week's topic is</h2>
+    <h1 class="text-4xl text-white uppercase">{{ currentTopic }}</h1>
 
-        <!-- <h2 class="self-center text-xl text-light-grey -mb-2 uppercase">Next week:</h2>
-        <h2 class="self-center text-l text-light-grey uppercase">{{ nextTopic }}</h2> -->
+    <!-- SKILLS PERCENTAGES -->
+    <div class="flex flex-col w-full" v-if="isTeacher">
+      <div class="flex flex-col mt-2 w-full">
+        <ul id="skillsList" class="-space-y-2">
+            <li v-for="(item, index) of skillsList" :key="index">
+              <span class="text-med-grey2 text-xs">{{ item }}</span>
+            </li>
+        </ul>
       </div>
     </div>
+
+  </div>
+</div>
 </template>
 
 <script>
-import { ref } from "vue"
+import { ref, reactive, onMounted } from "vue"
+import { getAllFocusLessons } from "../services/bjj_services/focusLessonService"
+import store from "../store/store";
 
 export default {
   name: "ThisWeek",
   setup() {
     const currentTopic = ref(null);
     const nextTopic = ref(null)
+    const skillsList = reactive([])
+    const isTeacher = ref(null)
+    const isStudent = ref(null)
+
+    function setRole() {
+      const role = store.state.user.role
+      if (role.admin || role.teacher) isTeacher.value = true
+      if (role.student) isStudent.value = true
+    }
 
     // **************  CURRENT & NEXT TOPIC **************
     // One topic per week
     const weeklyTopicList = [
-        "Back Control",
-        "Half Guard",
-        "Side Control",
-        "Closed Guard",
-        "Mount",
-        "De la Riva",
-        "Open Guard",
-        "Turtle"  
-    ]    
+        {"Back Control": "63476ca77c0c4048382acb04"},
+        {"Half Guard": "634ed31717260c95e351de8d"},
+        {"Side Control": "634ed53c17260c95e351decb"},
+        {"Closed Guard": "634ed77517260c95e351dfa3"},
+        {"Mount": "634ecefa9f04894fb818c868"},
+        {"De la Riva": "634edb2337829d81a79048ab"},
+        {"Open Guard": "638eaab8964f267814d40a89"},
+        {"Turtle": "638ead84964f267814d40ad7"},
+    ]
 
     // Gets the week number we are currently on since the startDate
     const currentWeekNumber = _ => {
@@ -48,12 +68,45 @@ export default {
     // Using modulus to "loop back" to index 0 after reaching last index
     const currentTopicArrayIndex = Math.floor(currentWeekNumber() % 8)
     const nextTopicArrayIndex = Math.floor(((currentWeekNumber() +1) % 8))
+
+    // Current Topic variables
+    const currentTopicObject = weeklyTopicList[currentTopicArrayIndex]
+    const currentTopicName = Object.keys(currentTopicObject)[0]
+    const currentTopicId = Object.values(currentTopicObject)[0]
+
+    // Next Topic variables
+    const nextTopicObject = weeklyTopicList[nextTopicArrayIndex]
+    const nextTopicName = Object.keys(nextTopicObject)[0]
+    const nextTopicId = Object.values(nextTopicObject)[0]
+
     // Assigning values to variables
-    currentTopic.value = weeklyTopicList[currentTopicArrayIndex]
-    nextTopic.value = weeklyTopicList[nextTopicArrayIndex]
+    currentTopic.value = currentTopicName
+    nextTopic.value = nextTopicName
+
+
+    // **************  SKILLS PERCENTAGES **************
+    async function getSkills(focusLessonId) {
+      const allFocusLessons = await getAllFocusLessons()
+      return allFocusLessons.filter(_id => JSON.stringify(_id).includes(focusLessonId))[0].skills
+    }
+
+    function populateSkillsList(skillsObject) {
+      for (const property in skillsObject) {
+        if (skillsObject[property] !== 0) skillsList.push(`${property}: ${skillsObject[property]}%`)
+      }
+    }
+    
+    onMounted(async() => {
+      setRole()
+      const currentTopicSkills = await getSkills(currentTopicId)
+      populateSkillsList(currentTopicSkills)
+    })
 
     return {
-      currentTopic, nextTopic
+      // TOPIC
+      currentTopic, nextTopic,
+      // SKILLS PERCENTAGES
+      skillsList, isTeacher, isStudent
     };
   }
 };
