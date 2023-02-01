8574c5ad8128872a70a87415dd7e76aa85a9f5e2
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-01 11:52:29 -0500

    remove unused vars

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<template>
-  <div v-if="appReady" class="min-h-full font-Poppins box-border bg-dark-grey">
-      <Navigation :key="navRerenderKey" />
-      <BottomNav :key="navRerenderKey" :role='userRole'/>
+<template >
+  <div class="min-h-screen font-Poppins box-border bg-dark-grey">
+    <Navigation />
+    <BottomNav :key="appReady" :role='userRole'/>
   </div>
 </template>
 
 <script>
 import BottomNav from "./components/BottomNav.vue";
 import Navigation from "./components/Navigation.vue";
-import { ref } from "vue";
 import store from "./store/store.js";
 import { useRouter } from "vue-router";
-import { inject } from "vue"    // required for the emitter (EventBus)
+import { inject, ref } from "vue";    // required for the emitter (EventBus)
 
 export default {
   components: {
@@ -20,32 +19,34 @@ export default {
   },
 
   setup() {
-    // Data & variables
-    const appReady = ref(null)
-    const router = useRouter()
+    //  Data & variables
     const user = JSON.parse(localStorage.getItem("BJJFocusUser"))
+    const router = useRouter()
     const userRole = ref('')
-    const navRerenderKey = ref(0) // works alongside the listener/emitter
-
-    // Listener (EventBus) this section listens to the emitters
     const emitter = inject('emitter')
-    emitter.on('userHasLoggedIn', (value) => {
-        navRerenderKey.value += 1
-        console.log("(emitter) Logged In")
+    const appReady = ref(0) // re-render key
+
+    //  Listener (EventBus) this section listens to the emitters
+    //
+    //  on user login or logout modifies appReady's value, 
+    //  with the intent that this will trigger a re-render of the BottomNav component
+    //  Re-render of the component however is not happening
+    emitter.on('userHasLoggedIn', (booleanValue) => {
+        appReady.value++
+        console.log('login')
     })
-    emitter.on('userHasLoggedOut', (value) => {
-        navRerenderKey.value += 1
-        console.log("(emitter) Logged Out")
+    emitter.on('userHasLoggedOut', (booleanValue) => {
+        appReady.value++
+        console.log('logout')
     })
 
+    //  Checks for user on page load
     if (!user) {
-      appReady.value = true;
-      store.methods.setUser(user);
       router.push({ name: "Login" });
-    } else {
-      appReady.value = true;
       store.methods.setUser(user);
-      // If user logs in, checks for their role to display relevant reoute and bottom nav bar
+    } else {
+      store.methods.setUser(user);
+      //  If user is logged in, checks for their role to display relevant route and bottom nav bar
       if(user.role.admin) {
         userRole.value = "admin"
         router.push({ name: "Session" });
@@ -56,11 +57,18 @@ export default {
       }
     }
 
-    return { appReady, user, emitter, navRerenderKey, userRole };
+    return { user, emitter, appReady, userRole };
   },
 };
 </script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap");
+@import "~primevue/resources/primevue.min.css";
+@import "~primevue/resources/themes/md-dark-deeppurple/theme.css";
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
