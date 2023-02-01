de7b7642e6b1df535e0f9d82b68bd469bd796a37
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-01 11:52:13 -0500

    add strictQuery

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', true)
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-
+        
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
         console.log(error)
