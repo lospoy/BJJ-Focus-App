0794456321c07fac31617c7c4f301e948042bb26
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-01 11:53:48 -0500

    add file

---
+++ src/store/user.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+
+
+
+export const useUserStore = defineStore("user", {
+  state: ()  => {
+    return { user: null }
+  },
+  actions: {
+    setUser(user) {
+      this.user = user
+    }
+  },
+  getters: {
+    getUser(user) {
+      return user
+    }
+  }
+})
