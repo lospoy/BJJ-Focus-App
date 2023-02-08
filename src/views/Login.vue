bb13c348a46f6b490b968864e3a960686efff6f9
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-08 15:49:03 -0500

    cleanup, add comments

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -52,9 +52,8 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { loginUser } from "../services/userService";
-import store from "../store/store"
-import { inject } from "vue"        // required for the emitter (EventBus)
 import Button from "../components/Button.vue";
+import { useUserStore } from "../store/user"
 
 export default {
   name: "login",
@@ -62,16 +61,8 @@ export default {
     Button,
   },
   setup() {
-    // Emitter (EventBus) emits an event that can be listened to globally
-    const emitter = inject('emitter')
-    const emitLogin = _ => {
-        emitter.emit('userHasLoggedIn', true)
-    }
-
-    // Tools
     const router = useRouter();
-
-    // Variables
+    const userStore = useUserStore()
     const email = ref(null);
     const password = ref(null);
     const errorMsg = ref(null);
@@ -79,14 +70,15 @@ export default {
     let buttonTitle = ref("Login")
 
     // Button success visual feedback
-    const buttonSuccess = async () => {
+    function buttonSuccess() {
         buttonTitle.value = "Logging in..."
         buttonColor.value = "orange"
     }
 
     // Login function
     const login = async () => {
-        await buttonSuccess()
+        // Button success visual feedback
+        buttonSuccess()
       try {
         const res = await loginUser({
           email: email.value,
@@ -95,17 +87,28 @@ export default {
 
         const userData = await res.json()
 
-        if (res.status === 200) {
+        //  If successful login:
+        if(res.status === 200) {
+            //  Set user data to local storage
             localStorage.setItem("BJJFocusUser", JSON.stringify(userData));
-            store.methods.setUser(userData)
-            const user = store.state.user
-            setTimeout(() => {
-                emitLogin() // emits the login event
-                if(user.role.student) router.push({ name: "StudentHome" })
-                if(user.role.admin) router.push({ name: "Session" })
-            }, 600);                
-        }
 
+            //  Set user data to userStore (Pinia @"../store/user")
+            //  If not placed inside of a timeout, app displays "Error: incorrect login"
+            //  but on browser refresh, you've logged in
+            //  network tab shows a 200 and a 204
+
+            //  Inside of the timeout, the app does not display "Error: incorrect login"
+            //  redirects correctly to "StudentHome", but userStore.user is null
+            //  and therefore does not load the bottom navigation component
+            setTimeout(() => {
+              userStore.setUser(userData)
+            }, 300);
+
+            setTimeout(() => {
+              if(userData.role.student) router.push({ name: "StudentHome" })
+              if(userData.role.admin) router.push({ name: "Session" })
+            }, 700);
+        }
       } catch (error) {
         errorMsg.value = "Error: incorrect login";
         setTimeout(() => {
@@ -114,7 +117,11 @@ export default {
       }
     };
 
-    return { email, password, errorMsg, login, buttonColor, buttonTitle, buttonSuccess, emitLogin };
+    return {
+      errorMsg,
+      buttonColor, buttonTitle, buttonSuccess,
+      email, password, login, userStore
+    };
   },
 };
 </script>
