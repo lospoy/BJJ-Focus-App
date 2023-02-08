113623f280184f507c1a752f1fef71c5d4f6689d
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-08 15:48:23 -0500

    add adminMenu and userStore

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -4,19 +4,26 @@
       :model="studentMenu"
       v-if="isStudent"
     />
+    <TabMenu
+      :model="adminMenu"
+      v-if="isAdmin"
+    />
     <router-view />
 </template>
 
 <script>
 import store from "../store/store"
-import { inject, ref } from "vue"        // required for the emitter (EventBus)
+import { inject, ref, onMounted } from "vue"        // required for the emitter (EventBus)
 import TabMenu from 'primevue/tabmenu';
+import { useRouter } from 'vue-router'
+import { logoutUser }  from '../services/userService'
+import { useUserStore } from "../store/user"
 
 export default {
 name: "BottomNav",
 props: {
   role: {
-    type: String,
+    type: Object,
     required: true,
   },
   components: {
@@ -31,6 +38,7 @@ setup(props) {
   const isAdmin = ref(null)
   const isStudent = ref(null)
   const active = ref(3)
+  const userStore = useUserStore()
   
   // TabMenu
   const studentMenu = ref([
@@ -52,6 +60,32 @@ setup(props) {
     },
   ])
 
+  const adminMenu = ref([
+    {
+      icon: 'pi pi-fw pi-home',
+      to: '/teacher/progress'
+    },
+    {
+      icon: 'pi pi-fw pi-calendar-plus',
+      to: '/teacher/session'
+    },
+    {
+      icon: 'pi pi-fw pi-user-plus',
+      to: '/teacher/human'
+    },
+    {
+      icon: 'pi pi-fw pi-sitemap',
+      to: '/teacher/technique'
+    },
+    {
+      icon: 'pi pi-fw pi-users',
+      to: '/teacher/student-list'
+    },
+    {
+      icon: 'pi pi-fw pi-power-off',
+      command:() => {logout()}
+    },
+  ])
 
   // Emitter (EventBus) this section emits an event that can be listened to globally
   const emitter = inject('emitter')
@@ -72,9 +106,23 @@ setup(props) {
     }, 700);
   };
 
+  // USING PINIA
+  function checkRole() {
+    if (!userStore.user) {
+      return
+    }
 
-  if(props.role === 'admin') isAdmin.value = true
-  if(props.role === 'student') isStudent.value = true
+    if(userStore.user.role.admin) isAdmin.value = true
+    if(userStore.user.role.student) isStudent.value = true
+  }
+
+  // USING PROPS
+  // if(props.role.admin) isAdmin.value = true
+  // if(props.role.student) isStudent.value = true
+
+  onMounted(() => {
+    checkRole()
+  })
 
 
   return {
@@ -82,7 +130,7 @@ setup(props) {
     // LOGOUT
     logout, emitLogout,
     // TAB MENU
-    studentMenu, isAdmin, isStudent, active
+    studentMenu, adminMenu, isAdmin, isStudent, active
   };
 },
 
