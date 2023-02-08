bf6e551783b8cb5f24194d38e31d2b68886cfa83
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-08 15:48:41 -0500

    define using composition API

--- src/store/user.js
+++ src/store/user.js
@@ -1,19 +1,24 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
+export const useUserStore = defineStore('user', () => {
+  const user = ref({
+    userID: '',
+    email: '',
+    humanID: '',
+    role: {}
+  })
 
+  function setUser(payload) {
+    if(!payload) { return user.value = null }
 
-export const useUserStore = defineStore("user", {
-  state: ()  => {
-    return { user: null }
-  },
-  actions: {
-    setUser(user) {
-      this.user = user
-    }
-  },
-  getters: {
-    getUser(user) {
-      return user
-    }
+    user.value.userID = payload._id,
+    user.value.email = payload.email,
+    user.value.humanID = payload.human,
+    user.value.role = payload.role
+  }
+
+  return {
+    user, setUser
   }
 })
