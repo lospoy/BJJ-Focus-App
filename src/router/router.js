ed299cdf4d653812a1c90d90d6ab3db132ac14de
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-15 12:06:59 -0500

    change vue-router history mode

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Human from "../views/Human.vue";
 import Login from "../views/Login.vue";
 import PrivateClass from "../views/PrivateClass.vue";
@@ -81,8 +81,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  mode: "hash",
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
